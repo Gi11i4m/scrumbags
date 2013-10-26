32440272135f576f5f42e0e1e26dc08acc7c4d63
Yens Proost <Yens.proost@gmail.com>
2013-10-26 14:23:59 +0200

    Why you no work commit

---
+++ Scrumbags Project/Scrumbags Project/RegistrationMail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scrumbags_Project
+{
+    public class RegistrationMail
+    {
+        public RegistrationMail()
+        { 
+        }
+
+        public void Send()
+        {
+            Mailing mail = new Mailing();
+            mail.to = "Yolan@Coeman.net";
+            mail.subject = "U piemel is net 2cm gekrompen.";
+            mail.body = "Buy my Penis enlarging product now!";
+            mail.send();
+        }
+
+        static void Main(string[] args)
+        {
+            RegistrationMail r = new RegistrationMail();
+            r.Send();
+               
+        }           
+   }
+
+    
+}
