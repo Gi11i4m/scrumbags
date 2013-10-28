541dc19409947e984afa214c23da4a013b236184
unknown <damenstijn@gmail.com>
2013-10-29 00:57:57 +0100

    Database aangemaakt, dbconnection klasse, en dbqueries klasse

---
+++ Scrumbags/Scrumbags/DBConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Scrumbags
+{
+    public class DBConnection
+    {
+        public static DataTable executeGetQuery(string query)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            conn.Close();
+
+            return dt;
+        }
+
+        public static void executeQuery(string query)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = query;
+            cmd.Connection = con;
+            con.Close();
+        }
+    }
+}
---
+++ Scrumbags/Scrumbags/DBQueries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scrumbags
+{
+    public class DBQueries
+    {
+        public static void Registrate()
+        {
+            DBConnection.executeQuery("INSERT INTO lecturers");
+        }
+    }
+}
