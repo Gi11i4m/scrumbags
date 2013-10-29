3b798a93bb3347e3e650123d3a38fa915439861e
Girmi Schouten <girmi.schouten@gmail.com>
2013-10-29 16:53:33 +0100

    Registration page add + working

---
+++ Scrumbags/Scrumbags/Registration.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Scrumbags
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            firstNameLabel.Text = "First Name: ";
+            lastNameLabel.Text = "Last Name: ";
+            emailLabel.Text = "Email address: ";
+            password1Label.Text = "Password: ";
+            password2Label.Text = "Repeat password: ";
+        }
+
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
