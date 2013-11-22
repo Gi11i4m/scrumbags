c41d901ab4ea97442515e40f68f0e9b4e6820b1b
Gilliam F <gi11i4m@gmail.com>
2013-11-22 15:25:11 +0100

    Master FIX, wil iedereen ff dit binnenhalen?

--- Scrumbags/Scrumbags/Home.aspx.cs
+++ Scrumbags/Scrumbags/Home.aspx.cs
@@ -17,17 +17,54 @@ namespace Scrumbags
                 Server.Transfer("Default.aspx", true);
             }
 
+            if (!Page.IsPostBack)
+            {
+                SlotsDataGrid.DataSource = DBQueries.getSlots();
+                SlotsDataGrid.DataBind();
+            }
+           
+            
+            
+            
+            
             // Use this statement to check if the user is an Admin
             if ((bool)Session["isAdmin"])
             {}
         }
 
-        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        protected void SlotsDataGrid_OnItemCommand(object sender, DataGridCommandEventArgs e)
         {
-            DBQueries.Reserve(int.Parse(Session["id"].ToString()), int.Parse(GridView1.SelectedRow.Cells[8].Text));
-            GridView1.DataBind();
-            //Label1.Text =GridView1.SelectedRow.Cells[8].Text;
+            DBQueries.Reserve(int.Parse(Session["id"].ToString()), int.Parse(e.Item.Cells[7].Text));
+            SlotsDataGrid.DataSource = DBQueries.getSlots();
+            SlotsDataGrid.DataBind();
+            
         }
+
+        protected void SlotsDataGrid_ItemDataBound(object sender, DataGridItemEventArgs e)
+        {
+            switch (e.Item.ItemType)
+            {
+                case ListItemType.AlternatingItem:
+
+                case ListItemType.Item:
+                    if (e.Item.Cells[6].Text.Equals("SubHeading"))
+                    {
+                        DateTime myDate = DateTime.Parse(e.Item.Cells[0].Text.ToString());
+                        e.Item.Cells[1].Text = myDate.DayOfWeek.ToString() + " " + myDate.Day + " " + myDate.Month;
+                        
+                        //De breedte van de toegevoegde subheader
+                        e.Item.Cells[1].ColumnSpan = e.Item.Cells.Count;
+
+                        //De overigge cellen verwijderen
+                        for (int i = e.Item.Cells.Count - 1; i > 1; i--)
+                            e.Item.Cells.RemoveAt(i);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
 
         protected void LogoutButton_Click(object sender, EventArgs e)
         {
@@ -41,5 +78,19 @@ namespace Scrumbags
                 Console.WriteLine(ex.Message);
             }
         }
+        protected void gvEmp_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "SelectSlots")
+            { }
+        }
+
+        protected void SlotsDataGrid_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SlotsDataGrid.DataSource = DBQueries.getSlots();
+            SlotsDataGrid.DataBind();
+            Label1.Text = "blap";
+        }
+
+       
     }
 }
