c83597aafb8d13933bd767afd246ac1facdd5042
Gilliam F <gi11i4m@gmail.com>
2013-12-13 16:11:49 +0100

    Design improvements

--- Scrumbags/Scrumbags/Reservations.aspx.designer.cs
+++ Scrumbags/Scrumbags/Reservations.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace Scrumbags {
     public partial class Reservations {
         
         /// <summary>
-        /// HomePageButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button HomePageButton;
-        
-        /// <summary>
         /// GridView1 control.
         /// </summary>
         /// <remarks>
