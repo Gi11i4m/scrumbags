2036097b6ed55221f5b10d3ba0d9eec1c86c93c1
Stijn D <damenstijn@gmail.com>
2013-12-27 00:50:36 +0100

    DBQueries testing

--- Scrumbags/UnitTestScrumbags/UnitTest1.cs
+++ Scrumbags/UnitTestScrumbags/UnitTest1.cs
@@ -7,10 +7,5 @@ namespace UnitTestScrumbags
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            DBConnection.executeQuery("SELECT COUNT(*) FROM LECTURERS");
-        }
     }
 }
